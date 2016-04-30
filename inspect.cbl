e2f002d275bd7138af6044780304fd47426ff30d
Nathaniel Palattao <nathaniel.palattao1@yahoo.com>
2016-04-30 12:51:44 +0800

    Update inspect.cbl

--- inspect.cbl
+++ inspect.cbl
@@ -3,32 +3,18 @@
  Environment Division.
  Data Division.
  Working-Storage Section.
- 01   Phone-number.
-    03  Area-code       Pic XXX value "409".
-    03  Prefix-num      Pic XXX value "555".
-    03  Last-four       Pic X(4)  value "1212".
- 01   Formatted-Number      Pic X(14) value "(XXX) YYY-ZZZZ".
- 01   Formatted-Alternate   Pic X(14) value "THIS IS SPARTA".
- 01   CCOUNT   Pic 99 value 00.
+ 01 Phone-number.
+ 03 Area-code Pic XXX value "409".
+ 03 Prefix-num Pic XXX value "555".
+ 03 Last-four Pic X(4) value "1212".
+ 01 Formatted-Number Pic X(14) value "(XXX) YYY-ZZZZ".
+ 01 Formatted-Alternate Pic X(14) value "THIS IS SPARTA".
  Procedure Division.
  Start-of-program.
-    Inspect Formatted-Number
-        Replacing All "XXX" by Area-code
-                All "YYY" by Prefix-num
-                All "ZZZZ" by Last-four.
-    Display Formatted-Number.
-    Inspect Formatted-Alternate
-        Replacing FIRST "XXX" by Area-code
-                First "XXX" by Prefix-num
-                First "XXXX" by Last-four.
-    Display Formatted-Alternate.
+ Inspect FORMATTED-ALTERNATE
+     Converting "QWERTYUIOPASDFGHJKLZXCVBNM" 
+              TO "abcdefghijklmnopqrstuvwxyz".
 
-    Inspect FORMATTED-ALTERNATE 
-        Replacing ALL "-" by "*".
+ Display Formatted-Alternate (4:1).
 
-    DISPLAY FORMATTED-ALTERNATE.
-    Inspect FORMATTED-ALTERNATE
-            Converting "QWERTYUIOPASDFGHJKLZXCVBNM" 
-            TO "abcdefghijklmnopqrstuvwxyz".
-    DISPLAY FORMATTED-ALTERNATE.
-    Stop Run.
+   Stop Run.
