0f0a3e475230bc477743d1c8c67a0dc93308965d
Nathaniel Palattao <nathaniel.palattao1@yahoo.com>
2016-04-30 11:56:59 +0800

    Create inspect.cbl

---
+++ inspect.cbl
@@ -0,0 +1,24 @@
+ Identification Division.
+ Program-Id. Inspect-sample.
+ Environment Division.
+ Data Division.
+ Working-Storage Section.
+ 01   Phone-number.
+    03  Area-code       Pic XXX value "409".
+    03  Prefix-num      Pic XXX value "555".
+    03  Last-four       Pic X(4)  value "1212".
+ 01   Formatted-Number      Pic X(14) value "(XXX) YYY-ZZZZ".
+ 01   Formatted-Alternate   Pic X(14) value "(XXX) XXX-XXXX".
+ Procedure Division.
+ Start-of-program.
+    Inspect Formatted-Number
+        Replacing All "XXX" by Area-code
+                All "YYY" by Prefix-num
+                All "ZZZZ" by Last-four.
+    Display Formatted-Number.
+    Inspect Formatted-Alternate
+        Replacing First "XXX" by Area-code
+                First "XXX" by Prefix-num
+                First "XXXX" by Last-four.
+    Display Formatted-Alternate.
+    Stop Run.
