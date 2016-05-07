79ed36b09f16eee6f5cbd698ed84b88820ab7900
Nathaniel Palattao <nathaniel.palattao1@yahoo.com>
2016-05-07 12:24:47 +0800

    Update if.cbl

--- if.cbl
+++ if.cbl
@@ -1,29 +1,36 @@
- IDENTIFICATION DIVISION.
- PROGRAM-ID. CONDITIONALS.
- ENVIRONMENT DIVISION.
- DATA DIVISION.
- WORKING-STORAGE SECTION.
- 01 XNUM1 PIC 9(3).
- 01 XNUM2 PIC 9(3).
- 01 XNUM3 PIC 9(3).  
- PROCEDURE DIVISION.
- EVALUATE-RTN.
- 	DISPLAY ERASE.
- 	DISPLAY "ENTER NUM-1 : " LINE 5 COL 5.
- 	DISPLAY "ENTER NUM-2 : " LINE 6 COL 5.
- 	DISPLAY "ENTER NUM-3 : " LINE 7 COL 5.
- 
- 	ACCEPT XNUM1 LINE 5 COL 19.
- 	ACCEPT XNUM2 LINE 6 COL 19.
- 	ACCEPT XNUM3 LINE 7 COL 19.	
- 	
- 	IF XNUM1 > XNUM2 AND XNUM1 > XNUM3
- 	DISPLAY XNUM1 LINE 9 COL 5.
- 	
- 	IF XNUM2 > XNUM1
- 	DISPLAY XNUM2 LINE 9 COL 5.
- 	
- 	IF XNUM3 > XNUM1
- 	DISPLAY XNUM3 LINE 9 COL 5.
- 	
- 	STOP RUN.
+ identification division.
+       program-id. conditionals.
+       environment division.
+       data division.
+       working-storage section.
+       01 xnuml pic 9(3).
+       01 xnum2 pic 9(3).
+       01 xnum3 pic 9(3).
+       procedure division.
+       evaluate-rtn.
+       display erase.
+       display "enter num-l:" line 5 col 5.
+       display "enter num-2:" line 6 col 5.
+       display "enter num-3:" line 7 col 5.
+      
+       accept xnuml line 5 col 19.
+       accept xnum2 line 6 col 19.
+       accept xnum3 line 7 col 19.
+* GET HIGHEST
+       if xnuml > xnum2 and xnuml > xnum3
+       	display xnuml line 9 COL 5.
+       IF xnum2 > xnuml AND XNUM2 > XNUM3
+       	display xnum2 line 9 COL 5.
+       IF xnum3 > xnuml AND XNUM3 > XNUM2
+       	display xnum3 line 9 COL 5.
+       	
+* CHECK FOR CLASS
+       IF XNUM3 IS NUMERIC
+       	DISPLAY "XNUM3 IS NUMERIC" LINE 10 COL 5.
+      
+* COMPOUND CONDITION
+       IF XNUM3 > 499 AND <1000
+       	DISPLAY "XNUM3 BETWEEN 499 AND 1000" LINE 11 COL 5.
+      
+      
+       STOP RUN.
